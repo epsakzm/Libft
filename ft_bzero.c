3c80ab279d8f1756fa4e260a874b01af7c1edbf0
박형우 <hwpark@MacBookPro.local>
2020-12-28 17:37:56 +0900

    eeeeeeeeeeeeeeeeeee

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: hyeopark <hyeopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 15:13:16 by hyeopark          #+#    #+#             */
-/*   Updated: 2020/12/23 15:17:50 by hyeopark         ###   ########.fr       */
+/*   Updated: 2020/12/28 17:28:56 by hyeopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,5 @@
 
 void		ft_bzero(void *s, size_t n)
 {
-	size_t		i;
-
-	if (!s)
-		return ;
-	i = 0;
-	while (i < n)
-	{
-		*(char*)(s + i) = 0;
-		i++;
-	}
+	ft_memset(s, 0, n);
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hyeopark <hyeopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 21:56:14 by hyeopark          #+#    #+#             */
-/*   Updated: 2020/12/26 14:01:27 by hyeopark         ###   ########.fr       */
+/*   Updated: 2020/12/28 17:33:33 by hyeopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 
 	if (!(ptr = malloc(count * size)))
-		return (NULL);
-	ft_bzero(ptr, size);
+		return (0);
+	ft_bzero(ptr, size * count);
 	return (ptr);
 }
