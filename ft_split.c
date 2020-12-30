669b501f79a6440154e164ef857ccbf33887b24e
박형우 <hwpark@MacBookPro.local>
2020-12-31 01:44:31 +0900

    split

--- ft_split.c
+++ ft_split.c
@@ -6,94 +6,89 @@
 /*   By: hyeopark <hyeopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 23:22:53 by hyeopark          #+#    #+#             */
-/*   Updated: 2020/12/30 23:32:11 by hyeopark         ###   ########.fr       */
+/*   Updated: 2020/12/31 01:44:15 by hyeopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_is_charset(char c, char charset)
+static char		**ft_free(char **arr)
 {
-	return (c == charset);
-}
-
-int		count_words(char const *str, char charset)
-{
-	int		count;
-
-	count = 0;
-	while (*str)
-	{
-		while (*str && ft_is_charset(*str, charset))
-			str++;
-		if (*str && !ft_is_charset(*str, charset))
-		{
-			count++;
-			while (*str && !ft_is_charset(*str, charset))
-				str++;
-		}
-	}
-	return (count);
-}
-
-char	**ft_free(char **arr)
-{
-	unsigned int i;
+	size_t i;
 
 	i = 0;
 	while (arr[i])
 	{
 		free(arr[i]);
-		i++;
+		++i;
 	}
-	free(arr);
+	if (arr)
+		free(arr);
 	return (NULL);
 }
 
-char	*malloc_word(char const *str, char charset)
+static int		count_words(char const *s, char c)
 {
-	int		i;
-	char	*word;
+	int		cnt;
+
+	cnt = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			++s;
+		if (*s && *s != c)
+		{
+			++cnt;
+			while (*s && *s != c)
+				++s;
+		}
+	}
+	return (cnt);
+}
+
+static char		*next_word(char const *s, char c)
+{
+	size_t		i;
+	char		*word;
 
 	i = 0;
-	while (str[i] && !ft_is_charset(str[i], charset))
-		i++;
+	while (s[i] && s[i] != c)
+		++i;
 	if (!(word = ft_calloc(i + 1, sizeof(char))))
 		return (NULL);
 	i = 0;
-	while (str[i] && !ft_is_charset(str[i], charset))
+	while (s[i] && s[i] != c)
 	{
-		word[i] = str[i];
-		i++;
+		word[i] = s[i];
+		++i;
 	}
-	word[i] = '\0';
+	word[i] = 0;
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+char			**ft_split(char const *s, char c)
 {
-	int		i;
-	char	**arr;
+	size_t	i;
+	char	**array;
 
 	if (!s)
-		return (0);
+		return (NULL);
+	if (!(array = ft_calloc(count_words(s, c) + 1, sizeof(char*))))
+		return (NULL);
 	i = 0;
-	if (!(arr = ft_calloc(count_words(s, c) + 1, sizeof(char *))))
-		return (0);
 	while (*s)
 	{
-		while (*s && ft_is_charset(*s, c))
-			s++;
-		if (*s && !ft_is_charset(*s, c))
+		while (*s && *s == c)
+			++s;
+		if (*s && *s != c)
 		{
-			arr[i] = malloc_word(s, c);
-			if (!arr[i])
-				return (ft_free(arr));
-			i++;
-			while (*s && !ft_is_charset(*s, c))
-				s++;
+			if (!(array[i] = next_word(s, c)))
+				return (ft_free(array));
+			++i;
+			while (*s && *s != c)
+				++s;
 		}
 	}
-	arr[i] = NULL;
-	return (arr);
+	array[i] = NULL;
+	return (array);
 }
